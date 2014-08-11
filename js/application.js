9d225103c9fb41e119ed38f5b3905b47926c39eb
pmcruzm <pmcruzm@Imac-de-Pedro-2.local>
2014-08-11 21:30:35 +0100

    Cambios 11-8-2014
    * Añade Slider Home
    * Terminado categoria.html
    * Terminado  recursos.html
    * Modificaciones catalogo.html

--- js/application.js
+++ js/application.js
@@ -9,6 +9,11 @@ Cliente: Cambridge University Press
 /**********************
 VARIABLES
 **********************/
+var pag_slider=1;
+var total_slider=0,width_scroll=0;
+
+
+
 
 jQuery.noConflict();
 
@@ -36,6 +41,14 @@ jQuery(document).ready(function(){
 	
 	//Comprobar que solo se carga en la home
 	if ( jQuery("#slider").is(":visible") ) {
+		
+		//Ajustamos los bloques contenidos slider 
+		total_slider=jQuery(".scroll-slider div[id^='slider_']").length;
+		var width_slider=jQuery("#body-slider").width();
+		var aux=total_slider*width_slider;
+		jQuery(".scroll-slider").css('width',(total_slider*width_slider));
+		jQuery(".inside-slider").css('width',(width_slider));
+		
 		//Galería de la Home
 		var slider=jQuery('.bxslider').bxSlider({
 						  /*mode: 'horizontal',
@@ -47,7 +60,87 @@ jQuery(document).ready(function(){
 						  useCSS: false,*/
 						  pager: false,
 						});
+										
 	}
+	
+	//Avanzar a la siguiente pantalla slider
+	jQuery(document).on("click",".arrow-next", function(e) {
+		e.preventDefault();
+		slider.goToNextSlide();
+		if(total_slider==pag_slider+1){
+			pag_slider=pag_slider+1; 
+			//Movemos contenidos
+			jQuery('#body-slider').stop().clearQueue().scrollTo(jQuery('#slider_'+pag_slider),400,{axis:'x',easing:'easeInOutExpo'});
+			//Deshabilitamos siguiente
+			jQuery('.arrow-next').css({visibility:'hidden'});
+		}else{
+			pag_slider=pag_slider+1;
+			//Movemos contenidos
+			jQuery('#body-slider').stop().clearQueue().scrollTo(jQuery('#slider_'+pag_slider),400,{axis:'x',easing:'easeInOutExpo'});
+			//pintamos el bullet correspondiente
+		}
+		//pintamos el bullet correspondiente
+		jQuery('#nav-slider ul li a').removeClass('active');
+		jQuery('#nav-slider ul li a[rel='+pag_slider+']').addClass('active');
+		//Habilitamos anterior
+		console.log(pag_slider);
+		jQuery('.arrow-prev').css({visibility:'visible'});	
+	});	
+		
+	//Retroceder a la siguiente pantalla del slider 
+	jQuery(document).on("click",".arrow-prev", function(e) {
+		e.preventDefault();
+			slider.goToPrevSlide();
+			if(pag_slider-1==1){
+				pag_slider=pag_slider-1;
+				jQuery('#body-slider').stop().clearQueue().scrollTo(jQuery('#slider_'+pag_slider),400,{axis:'x',easing:'easeInOutExpo'});
+				//Deshabilitamos siguiente
+				jQuery('.arrow-prev').css({visibility:'hidden'});			
+			}else{
+				pag_slider=pag_slider-1;
+				jQuery('#body-slider').stop().clearQueue().scrollTo(jQuery('#slider_'+pag_slider),400,{axis:'x',easing:'easeInOutExpo'});
+				//pintamos el bullet correspondiente
+			}
+			//pintamos el bullet correspondiente
+			jQuery('#nav-slider ul li a').removeClass('active');
+			jQuery('#nav-slider ul li a[rel='+pag_slider+']').addClass('active');
+			//Habilitamos siguiente
+			console.log(pag_slider);
+			jQuery('.arrow-next').css({visibility:'visible'});
+	});
+	
+	//Cuando pulsas sobre uno de los bullets del slider de la home
+	jQuery(document).on("click","#nav-slider ul li a", function(e) {
+		e.preventDefault();
+			var pag= jQuery(this).attr('rel');
+			if(pag!=='next' && pag!='prev'){
+				//Añadimos color al bullet 
+				jQuery('#nav-slider ul li a').removeClass('active');
+				jQuery(this).addClass('active');
+				pag=parseInt(pag);
+				//Comprobamos si es la primera página
+				if(pag==1){
+					slider.goToSlide(pag-1);
+					jQuery('#body-slider').stop().clearQueue().scrollTo(jQuery('#slider_'+pag),400,{axis:'x',easing:'easeInOutExpo'});
+					jQuery('.arrow-prev').css({visibility:'hidden'});
+					jQuery('.arrow-next').css({visibility:'visible'});
+				}else{
+					//Comprobamos si es la última
+					if(total_slider==pag){
+						slider.goToSlide(pag-1);
+						jQuery('#body-slider').stop().clearQueue().scrollTo(jQuery('#slider_'+pag),400,{axis:'x',easing:'easeInOutExpo'});
+						jQuery('.arrow-prev').css({visibility:'visible'});
+						jQuery('.arrow-next').css({visibility:'hidden'});
+					}else{
+						slider.goToSlide(pag-1);
+						jQuery('#body-slider').stop().clearQueue().scrollTo(jQuery('#slider_'+pag),400,{axis:'x',easing:'easeInOutExpo'});
+						jQuery('.arrow-prev').css({visibility:'visible'});
+						jQuery('.arrow-next').css({visibility:'visible'});
+					}
+				}
+				pag_slider=pag;
+			}
+	});
 	
 	
 });
